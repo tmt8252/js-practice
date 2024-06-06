444a434e34a686d1e0102c4eb25278bdf0b3b17a
Taher <tmttinwala8252@gmail.com>
2024-06-06 18:02:40 +0530

    api

--- fetchUsingMap/script.js
+++ fetchUsingMap/script.js
@@ -26,3 +26,44 @@ const nestingNavigation = [
   },
 ];
 
+const a = [1, 5, 3, 7, 9, 4];
+
+console.log(a.includes(10));
+
+console.log(JSON.stringify(nestingNavigation));
+
+// const addList = (items) => {
+//   const ul = document.createElement("ul");
+
+//   items.map((a) => {
+//     const li = document.createElement("li");
+//     li.innerHTML = a.title;
+
+//     if (a.values && Array.isArray(a.values)) {
+
+//     }
+//   });
+//   ul.appendChild(li);
+// };
+
+// const list = document.getElementById("list");
+
+// list.appendChild(addList(nestingNavigation));
+
+const list = document.getElementById("list");
+
+list.innerHTML = nestingNavigation
+  .map((a) => {
+    return `
+    <ul>
+      <li>${a.title}
+        <ul>
+          ${a.values.map((b) => {
+            return `<li>${b.title}</li>`;
+          })}
+        </ul>
+      </li>
+    </ul>
+  `;
+  })
+  .join("");
