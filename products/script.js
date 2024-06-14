54181e293e4adc3d0471c7d7e9683ab1729169c5
Taher <tmttinwala8252@gmail.com>
2024-06-14 17:45:05 +0530

    added cart nav

--- products/script.js
+++ products/script.js
@@ -1,12 +1,14 @@
 async function getData() {
   const rowData = await fetch(
-    "https://res.cloudinary.com/dpiiduvvx/raw/upload/v1718019896/API/ProductsData"
+    "https://res.cloudinary.com/dpiiduvvx/raw/upload/v1703580765/API/productsAPI"
   );
-  const obj = await rowData.json();
+  const { earbud, tshirt, shoes, tshirtWomen } = await rowData.json();
+
+  const dataArr = [...earbud, ...tshirt, ...shoes, ...tshirtWomen];
 
   const container = document.getElementById("container");
 
-  obj
+  dataArr
     .map((product) => {
       const card = document.createElement("div");
       card.classList.add("card");
@@ -16,7 +18,7 @@ async function getData() {
       card.appendChild(img1);
 
       const img2 = document.createElement("img");
-      img2.src = product.img2;
+      img2.src = product.img3;
       card.appendChild(img2);
 
       const title = document.createElement("p");
@@ -24,7 +26,7 @@ async function getData() {
       card.appendChild(title);
 
       const price = document.createElement("p");
-      price.textContent = `Price - $${product.price}`;
+      price.textContent = `Price - Rs.${product.price}`;
       card.appendChild(price);
 
       const discount = document.createElement("p");
@@ -39,14 +41,15 @@ async function getData() {
       category.textContent = `Category - ${product.category}`;
       card.appendChild(category);
 
-      const subCategory = document.createElement("p");
-      subCategory.textContent = `Sub Category - ${product.subCategory}`;
-      card.appendChild(subCategory);
-
       const button = document.createElement("button");
       button.textContent = "Add to Cart";
       card.appendChild(button);
 
+      button.addEventListener('click', () => {
+      
+        console.log("card")
+      
+      })
       container.appendChild(card);
 
       //! when user click add to cart button.. you have to store into localStorage and show into a
@@ -56,3 +59,9 @@ async function getData() {
 }
 
 getData();
+
+
+
+const addCart = () => {
+  console.log("cart");
+};
